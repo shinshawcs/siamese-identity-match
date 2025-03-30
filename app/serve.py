c129a3157f3fa1844b859e5dc82a60d7558f6bb9
Xin Xiao <xxiao@Xins-MacBook-Pro.local>
2025-03-30 15:47:03 -0500

    remove Userwarning

--- app/serve.py
+++ app/serve.py
@@ -3,9 +3,11 @@ from pydantic import BaseModel
 import torch
 import joblib
 import numpy as np
-from app.models.siamese_mlp import SiameseMLP
+from models.siamese_mlp import SiameseMLP
 import os
 import pandas as pd
+import warnings
+
 app = FastAPI()
 
 # 加载模型和 encoder
@@ -13,33 +15,43 @@ model_path = "checkpoints/siamese_model.pt"
 encoder_path = "checkpoints/encoder.pkl"
 
 encoder = joblib.load(encoder_path)
-input_dim = encoder.transform([encoder.feature_names_in_]).shape[1]
+required_columns = encoder.feature_names_in_.tolist()
+
+dummy_df = pd.DataFrame(columns=encoder.feature_names_in_)
+dummy_df.loc[0] = ["unknown"] * len(encoder.feature_names_in_)
+input_dim = encoder.transform(dummy_df).shape[1]
+
 model = SiameseMLP(input_dim=input_dim)
-model.load_state_dict(torch.load(model_path))
+model.load_state_dict(torch.load(model_path, map_location="cpu"))
 model.eval()
 
 class InputFeatures(BaseModel):
     a: dict
     b: dict
 def fill_missing_keys(d, keys, default="unknown"):
-    return {k: d.get(k, default) for k in keys}
+        return {k: d.get(k, default) for k in keys}
 
 @app.post("/predict")
-def predict(data: InputFeatures):
-    required_keys = encoder.feature_names_in_
+async def predict(data: InputFeatures):
 
-    a_fixed = fill_missing_keys(data.a, required_keys)
-    b_fixed = fill_missing_keys(data.b, required_keys)
+    try:
+        a_fixed = fill_missing_keys(data.a, required_columns)
+        b_fixed = fill_missing_keys(data.b, required_columns)
 
-    df_a = pd.DataFrame([a_fixed])
-    df_b = pd.DataFrame([b_fixed])
-    a = encoder.transform(df_a)
-    b = encoder.transform(df_b)
+        df_a = pd.DataFrame([a_fixed], columns=encoder.feature_names_in_)
+        df_b = pd.DataFrame([b_fixed], columns=encoder.feature_names_in_)
 
-    a = torch.tensor(a, dtype=torch.float32)
-    b = torch.tensor(b, dtype=torch.float32)
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            a = encoder.transform(df_a)
+            b = encoder.transform(df_b)
 
-    with torch.no_grad():
-        out = model(a, b).item()
+        a = torch.tensor(a, dtype=torch.float32)
+        b = torch.tensor(b, dtype=torch.float32)
 
-    return {"match_score": out}
+        with torch.no_grad():
+            out = model(a, b).item()
+
+        return {"match_score": out}
+    except Exception as e:
+        return {"error": str(e)}, 500
